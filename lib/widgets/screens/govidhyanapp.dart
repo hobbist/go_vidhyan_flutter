d1819a20a74aa8ae6b3f37f1b6a3fdb0673d077a
hobbist <kapil.21.kulkarni21@gmail.com>
2018-12-31 19:37:58 +0530

    adding login screen widget

--- lib/widgets/screens/govidhyanapp.dart
+++ lib/widgets/screens/govidhyanapp.dart
@@ -5,10 +5,25 @@ import 'package:govidhyan_flutter/widgets/screens/loginscreen.dart';
 class GoVidhyanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return MaterialApp(home:HomeScreen(),
+      theme: ThemeData(
+          primarySwatch: Colors.lightGreen,
+          primaryTextTheme: TextTheme(
+              headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+              title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.normal),
+              body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'))),
+    );
   }
 }
+
+class DrawerItem {
+  String title;
+  IconData icon;
+  DrawerItem(this.title, this.icon);
+}
+
 class HomeScreen extends StatefulWidget{
+  final drawerItems = [ new DrawerItem(login, Icons.alternate_email),];
   @override
   State<StatefulWidget> createState() {
     return HomeScreenState();
@@ -16,64 +31,61 @@ class HomeScreen extends StatefulWidget{
 
 }
 
-class HomeScreenState extends State<StatefulWidget>{
+class HomeScreenState extends State<HomeScreen>{
+  int _selectedDrawerIndex = 1;
+
+  _onSelectItem(int index) {
+    setState(() => _selectedDrawerIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
+
+  _getDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 1:
+        return new LoginScreen();
+      default:
+        return new Text("Error Text");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title:title,
-      home: Scaffold(
+    var drawerOptions = <Widget>[];
+    drawerOptions.add(DrawerHeader(
+      decoration: BoxDecoration(
+          image: DecorationImage(image: ExactAssetImage(imageLogoPath)),
+          color: Colors.lightGreen
+      ),
+    ));
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var d = widget.drawerItems[i];
+      i=i+1;
+      drawerOptions.add(
+          new ListTile(
+            leading: new Icon(d.icon),
+            title: new Text(d.title),
+            selected: i == _selectedDrawerIndex,
+            onTap: () => _onSelectItem(i),
+          )
+      );
+    }
+    drawerOptions.add(new ListTile(
+      leading: new Icon(Icons.language),
+      title: new Text(aboutUs),
+      onTap: () => launchURL(goVidhnyanUrl),
+    ));
+
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
         drawer: Drawer(
           child: ListView(
-            // Important: Remove any padding from the ListView.
             padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                    image: DecorationImage(image: ExactAssetImage(imageLogoPath)),
-                    color: Colors.lightGreen
-                ),
-              ),
-              ListTile(
-                title: Text(login,style: TextStyle(fontSize: 17)),
-                onTap: () {
-
-                },
-              ),
-              ListTile(
-                title: Text(profile,style: TextStyle(fontSize: 17),),
-                onTap: () {
-
-                },
-              ),
-              ListTile(
-                title: Text(stages,style: TextStyle(fontSize: 17)),
-                onTap: () {
-
-                },
-              ),
-              ListTile(
-                title: Text(contact_coordinator,style: TextStyle(fontSize: 17)),
-                onTap: () {
-
-                },
-              ),ListTile(
-                title: Text(aboutUs,style: TextStyle(fontSize: 17)),
-                onTap: () {
-                  launchURL(goVidhnyanUrl);
-                },
-              ),
-            ],
+            children: drawerOptions,
           ),
         ),
-        appBar: AppBar(title: Text(title),),
-      ),
-      theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
-          primaryTextTheme: TextTheme(
-              headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-              title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.normal),
-              body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'))),
-    );;
+
+      body: _getDrawerItemWidget(_selectedDrawerIndex),
+    );
   }
 
 }
--- lib/widgets/screens/loginscreen.dart
+++ lib/widgets/screens/loginscreen.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:govidhyan_flutter/widgets/constants/constants.dart';
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return LoginWidget();
+  }
+}
+
+class LoginWidget extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
     return LoginState();
   }
+
 }
 
-class LoginState extends State<LoginScreen>{
+
+class LoginState extends State<LoginWidget>{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: login,
-        home: Center(child: Text(login)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(children: <Widget>[
+          Flexible(child: TextField(
+            decoration: InputDecoration.collapsed(border:InputBorder.none,
+                hintText: 'Full Name e.g \'Narayan Jatkar\''),
+            autofocus: bool.fromEnvironment('true'),))
+        ],
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        )
+      ],
+
     );
   }
 }
+
